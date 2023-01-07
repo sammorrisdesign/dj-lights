4f41ac77bdaae75db4fafa65328d7c30889973bd
sammorrisdesign <mail@sammorrisdesign.co.uk>
2023-01-07 12:50:41 -0500

    just output lights

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const ws281x = require('rpi-ws281x-native');
 
 const channel = ws281x(100, { stripType: 'ws2812' });
 
+console.log('starting');
+
 const colorArray = channel.array;
 for (let i = 0; i < channel.count; i++) {
   colorArray[i] = 0xffcc22;
 }
+
+console.log(colorArray);
 
 ws281x.render();
