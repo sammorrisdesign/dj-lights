0d9f0f9d98fda3ad3c088903b82267574813d2a6
sammorrisdesign <mail@sammorrisdesign.co.uk>
2023-01-07 13:45:03 -0500

    create the loop

--- app.js
+++ app.js
@@ -1,47 +1,60 @@
 const ws281x = require('rpi-ws281x-native');
 const getColors = require('get-image-colors');
 
+// light config
 const lightCount = 50;
-const channel = ws281x(lightCount, { stripType: 'ws2812' });
+const lights = ws281x(lightCount, { stripType: 'ws2812' });
 
-const setLights = color => {
-  console.log('setting lights to', color);
-  const colorArray = channel.array;
-
-  for (let i = 0; i < channel.count; i++) {
-    colorArray[i] = 0xffcc22 // use color value here instead
-  }
-
-  ws281x.render();
-}
-
+// camera config
 const PiCamera = require('pi-camera');
-const myCamera = new PiCamera({
+const camera = new PiCamera({
   mode: 'photo',
   width: 640,
   height: 480,
   nopreview: true,
 });
 
-myCamera.snapDataUrl()
-  .then((result) => {
-    getColors(result, 'image/jpg').then(colors => {
-      const colorsAsHSL = colors.map(color => color.hsl());
+const setLights = color => {
+  console.log('setting lights to', color);
+  const colorArray = lights.array;
 
-      let highSaturationIndex = null;
+  for (let i = 0; i < lights.count; i++) {
+    colorArray[i] = 0xffcc22 // use color value here instead
+  }
 
-      colorsAsHSL.forEach((color, i) => {
-        if (color[1] && !highSaturationIndex) {
-          highSaturationIndex = i;
-        }
-      });
+  ws281x.render();
+}
 
-      const colorsAsHex = colors.map(color => color.hex());
-      const colorToSet = highSaturationIndex ? colorsAsHex[highSaturationIndex] : colorsAsHex[0];
+const takePhoto = () => {
+  camera.snapDataUrl()
+    .then((result) => {
+      getColors(result, 'image/jpg').then(colors => {
+        const colorsAsHSL = colors.map(color => color.hsl());
 
-      setLights(colorToSet);
+        let highSaturationIndex = null;
+
+        colorsAsHSL.forEach((color, i) => {
+          if (color[1] && !highSaturationIndex) {
+            highSaturationIndex = i;
+          }
+        });
+
+        const colorsAsHex = colors.map(color => color.hex());
+        const colorToSet = highSaturationIndex ? colorsAsHex[highSaturationIndex] : colorsAsHex[0];
+
+        setLights(colorToSet);
+      }).catch((error) => {
+        console.log(error);
+      })
     })
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log('done photoing');
+  setTimeout(() => {
+    takePhoto();
+  }, 500);
+}
+
+takePhoto();
