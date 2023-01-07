65cd69adad946b7e3af79d879d99401cf009aba5
sammorrisdesign <mail@sammorrisdesign.co.uk>
2023-01-07 12:53:57 -0500

    add camera code

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const ws281x = require('rpi-ws281x-native');
 
-const channel = ws281x(100, { stripType: 'ws2812' });
+const lightCount = 50;
+const channel = ws281x(lightCount, { stripType: 'ws2812' });
 
-console.log('starting');
+const setLights = color => {
+  console.log('setting lights to', color);
+  const colorArray = channel.array;
 
-const colorArray = channel.array;
-for (let i = 0; i < channel.count; i++) {
-  colorArray[i] = 0xffcc22;
+  for (let i = 0; i < channel.count; i++) {
+    colorArray[i] = 0xffcc22 // use color value here instead
+  }
+
+  ws281x.render();
 }
 
-console.log(colorArray);
+// setLights('red');
 
-ws281x.render();
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: `~/test.jpg`,
+  width: 640,
+  height: 480,
+  nopreview: true,
+});
+
+myCamera.snap()
+  .then((result) => {
+    console.log('captured');
+    // Your picture was captured
+  })
+  .catch((error) => {
+     // Handle your error
+  });
