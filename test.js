c6c010218d8e2849c315d5c1ce86384c145cd3f7
Sammorrisdesign <mail@sammorrisdesign.co.uk>
2025-08-02 16:18:07 -0400

    revert test to all

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const config = require('./config.json');
 const shell = require('shelljs');
 
 // taken from https://www.raspberrypi.com/documentation/computers/camera_software.html#awb
-// const awbs = ["auto", "incandescent", "tungsten", "fluorescent", "indoor", "daylight", "cloudy"];
-const awbs = ["auto"];
+const awbs = ["auto", "incandescent", "tungsten", "fluorescent", "indoor", "daylight", "cloudy"];
+// const awbs = ["auto"];
 
 for (const awb of awbs) {
   console.log('taking photo for', awb);
