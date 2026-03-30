52d55181872d50ae5dd85bc5d060a0ecde561f11
Sammorrisdesign <mail@sammorrisdesign.co.uk>
2026-03-30 19:48:18 -0400

    quiet the camera please

--- src/app.js
+++ src/app.js
@@ -155,7 +155,8 @@ const takePhoto = () => {
   console.time('taking photo');
 
   // Options from: https://www.raspberrypi.com/documentation/computers/camera_software.html#common-command-line-options
-  shell.exec(`rpicam-jpeg --nopreview -t 10 --immediate --shutter 600000 --gain 1.0 --lens-position 10  --output capture.jpg`)
+  shell.exec(`rpicam-jpeg --width 1920 --height 1080 --nopreview -t 10 --immediate --shutter 600000 --gain 1.0 --lens-position 10 --verbose 0 --output capture.jpg`)
+
   console.timeEnd('taking photo');
 
   getColorFromImage();
