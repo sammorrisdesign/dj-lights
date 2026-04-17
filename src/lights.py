016b533b415547a7ddc0653353a538467a7d54d4
sammorrisdesign <mail@sammorrisdesign.co.uk>
2026-04-17 17:33:10 -0400

    fix python script

--- src/lights.py
+++ src/lights.py
@@ -6,14 +6,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--r', dest='r', type=int, help='Red value of the colour you want')
 parser.add_argument('--g', dest='g', type=int, help='Green value of the colour you want')
 parser.add_argument('--b', dest='b', type=int, help='Blue value of the colour you want')
-parser.add_argument('--count', dest='count', type=int, help='LED Count')
-parser.add_argument('--brightness', dest='count', default=0.4, type=float, help='LED Count')
+parser.add_argument('--brightness', dest='brightness', default=0.4, type=float, help='Brightness')
 args = parser.parse_args()
 
-print("hello")
-
 # Initialize the WS2812 strip with 100 leds and SPI channel 0, CE0
-strip = WS2812SpiDriver(spi_bus=0, spi_device=0, led_count=args.count).get_strip()
+strip = WS2812SpiDriver(spi_bus=0, spi_device=0, led_count=150).get_strip()
 strip.set_all_pixels(Color(args.r, args.g, args.b))
 strip.set_brightness(args.brightness)
 strip.show()
