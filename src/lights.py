19afa83b050915372b6e88ca83bd7de6033d9ceb
Sammorrisdesign <mail@sammorrisdesign.co.uk>
2026-04-17 19:04:35 -0400

    try more

--- src/lights.py
+++ src/lights.py
@@ -2,6 +2,8 @@ from pi5neo import Pi5Neo
 import time
 import argparse
 
+print("lights")
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--r', dest='r', type=int, help='Red value of the colour you want')
 parser.add_argument('--g', dest='g', type=int, help='Green value of the colour you want')
@@ -9,6 +11,6 @@ parser.add_argument('--b', dest='b', type=int, help='Blue value of the colour yo
 parser.add_argument('--brightness', dest='brightness', default=0.4, type=float, help='Brightness')
 args = parser.parse_args()
 
-with Pi5Neo('/dev/spidev0.0', num_leds=150, spi_speed_khz=800, quiet_mode=True) as neo:
-    neo.fill_strip(args.r, args.g, args.b)
-    neo.update_strip()
+neo = Pi5Neo('/dev/spidev0.0', num_leds=150, spi_speed_khz=800)
+neo.fill_strip(255, 0, 0)   # Red
+neo.update_strip()
