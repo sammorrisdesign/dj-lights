79366a8cf5fee6c1fd38eaa235097cc49f57a19f
sammorrisdesign <mail@sammorrisdesign.co.uk>
2026-04-17 20:11:22 -0400

    use values in python

--- src/app.js
+++ src/app.js
@@ -70,7 +70,9 @@ const updateLights = (color = null) => {
     color = state.color;
   }
 
-  shell.exec(`python src/lights.py --r ${color[0]} --g ${color[1]} --b ${color[2]} --brightness ${state.brightness}`)
+  console.log('changing the color');
+
+  shell.exec(`python src/lights.py --r ${color[0]} --g ${color[1]} --b ${color[2]}`)
 }
 
 const extractor = await pipeline(
--- src/lights.py
+++ src/lights.py
@@ -8,9 +8,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--r', dest='r', type=int, help='Red value of the colour you want')
 parser.add_argument('--g', dest='g', type=int, help='Green value of the colour you want')
 parser.add_argument('--b', dest='b', type=int, help='Blue value of the colour you want')
-parser.add_argument('--brightness', dest='brightness', default=0.4, type=float, help='Brightness')
 args = parser.parse_args()
 
 neo = Pi5Neo('/dev/spidev0.0', num_leds=150, spi_speed_khz=800)
-neo.fill_strip(255, 0, 0)   # Red
+neo.fill_strip(args.r, args.g, args.b)   # Red
 neo.update_strip()
