d13b79029067562eaa35b10d42bb3bf96b94bba6
sammorrisdesign <mail@sammorrisdesign.co.uk>
2026-04-26 12:12:24 -0400

    better way to ignore individual albums

--- src/data.js
+++ src/data.js
@@ -5,16 +5,23 @@ import colors from "../data/colors.json" with { type: 'json' };
 import embedding from "../data/embedding.json" with { type: 'json' };
 
 let data = new Array;
+const ignoreList = [
+  18542482, // R.A.P. Ferreira - Bob's Son (pure black cover throws it off) 
+]
 
 for (const release of releases) {
   const embeddingForRelease = embedding.find(embed => embed.id == release.id).embedding;
   const colorForRelease = colors.find(color => color.id == release.id).color;
 
-  data.push({
-    ...release,
-    embedding: embeddingForRelease,
-    color: colorForRelease
-  });
+  if (!ignoreList.includes(release.id)) {
+   data.push({
+      ...release,
+      embedding: embeddingForRelease,
+      color: colorForRelease
+    });
+  } else {
+    console.log(`🫣 Ignoring ${release.title} – ${release.artist}`)
+  }
 }
 
 fs.writeFileSync("data/data.json", JSON.stringify(data, null, 2));
